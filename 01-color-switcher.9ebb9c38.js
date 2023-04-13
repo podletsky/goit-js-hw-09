5ff6c331f51bbff5bd357fbac50470abd747c397
podletsky <113206352+podletsky@users.noreply.github.com>
2023-04-13 08:23:13 +0000

    Deploying to gh-pages from @ podletsky/goit-js-hw-09@bdda12509b13d4e42ec7a67a029178a2ec5b0f9f 🚀

---
+++ 01-color-switcher.9ebb9c38.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){intervalId=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),n.addEventListener("click",(function(){clearInterval(intervalId)}));
+//# sourceMappingURL=01-color-switcher.9ebb9c38.js.map

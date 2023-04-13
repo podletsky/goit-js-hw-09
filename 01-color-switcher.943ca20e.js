4636a1c54315908e5f3e0ef0af7e235430371b38
podletsky <113206352+podletsky@users.noreply.github.com>
2023-04-13 09:10:53 +0000

    Deploying to gh-pages from @ podletsky/goit-js-hw-09@8a677c280047e98a4d05ddffe4474f136f3b6ced 🚀

--- 01-color-switcher.943ca20e.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){intervalId=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(intervalId)}))}();
-//# sourceMappingURL=01-color-switcher.943ca20e.js.map
---
+++ 01-color-switcher.cec28b10.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("body"),t=document.querySelector("[data-start]"),d=document.querySelector("[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,d.disabled=!1,intervalId=setInterval((()=>{e.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),d.addEventListener("click",(function(){t.disabled=!1,d.disabled=!0,clearInterval(intervalId)}));
+//# sourceMappingURL=01-color-switcher.cec28b10.js.map

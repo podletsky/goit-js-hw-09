bdda12509b13d4e42ec7a67a029178a2ec5b0f9f
podletsky <nazar.org@gmail.com>
2023-04-13 11:22:44 +0300

    color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,35 @@
 
+const body = document.querySelector('body')
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+ btnStart.addEventListener("click", startColorChange);
+  btnStop.addEventListener("click", stopColorChange);
+
+function startColorChange() {
+ intervalId = setInterval(() => {
+   body.style.backgroundColor = getRandomHexColor()
+    }, 1000);
+  }
+function stopColorChange() {
+    clearInterval(intervalId)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

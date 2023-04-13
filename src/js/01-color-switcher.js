660714f507d4143fc9f9b75735e8117cd3f40067
podletsky <nazar.org@gmail.com>
2023-04-13 13:24:28 +0300

    choice

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,7 +2,8 @@ const body = document.querySelector("body");
   const startButton = document.querySelector("[data-start]");
   const stopButton = document.querySelector("[data-stop]");
 let intervalId=null
-   
+       startButton.addEventListener("click", startColorChange);
+  stopButton.addEventListener("click", stopColorChange);
   function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
   }
@@ -19,6 +20,5 @@ let intervalId=null
     startButton.disabled = false;
     stopButton.disabled = true;
     clearInterval(intervalId);
-     startButton.addEventListener("click", startColorChange);
-  stopButton.addEventListener("click", stopColorChange);
+ 
   }
